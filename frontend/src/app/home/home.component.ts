011aa0e41130d2302841213bf42742327cd106e9
poojaShah <shah.developer9@gmail.com>
2020-04-23 20:16:20 +0530

    photo

--- frontend/src/app/home/home.component.ts
+++ frontend/src/app/home/home.component.ts
@@ -1,4 +1,4 @@
-import {Component, OnInit} from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import {HttpClient} from '@angular/common/http';
 import {FighterService} from "../services/fighter.service";
 import {DomSanitizer} from '@angular/platform-browser';
@@ -13,23 +13,22 @@ export class HomeComponent implements OnInit {
   display = 'none';
   imgName = '';
   cntName = '';
-  searchText = '';
   mySrc = '';
+  searchText = '';
   fighterData: any = {};
   showDD = false;
   countrylist: any = [];
 
   constructor(private http: HttpClient,
               private sanitizer: DomSanitizer,
-              private fighterService: FighterService) {
-  }
+              private fighterService: FighterService) { }
 
   ngOnInit(): void {
     // magnificPopup();
     this.http.get<any>('https://api.ipdata.co?api-key=test')
       .subscribe((res) => {
         console.log('location res', res);
-        if (res) {
+        if(res) {
           this.cntName = res.country_name;
           this.getFighter(this.cntName);
         }
@@ -42,7 +41,7 @@ export class HomeComponent implements OnInit {
 
   onTabClick(type) {
     console.log('type', type);
-    if (type === 'country') {
+    if(type === 'country') {
       this.getFighter(this.cntName);
     } else {
       this.getFighter('');
@@ -53,18 +52,12 @@ export class HomeComponent implements OnInit {
     this.fighterService.getFighterList(country)
       .subscribe((res) => {
         console.log('rees', res);
-        if (res.data) {
+        if(res.data) {
           this.fighterData = res.data;
           this.fighterData.filter((x) => {
-            console.log('dataaaaaaaa', x.photo.data.data);
-            const reader = new FileReader();
-            reader.onload = (e) => this.mySrc = e.target.result;
-            reader.readAsDataURL(new Blob([x.photo.data.data]));
-
-            console.log('this.mySrc', this.mySrc);
             let image = this._arrayBufferToBase64(x.photo.data.data);
-            this.myFunction('data:image/jpg;base64, ' + this._arrayBufferToBase64(x.photo.data.data));
-
+            // this.myFunction(image);
+            this.mySrc = image
           });
         }
       }, error => {
@@ -73,20 +66,16 @@ export class HomeComponent implements OnInit {
 
   }
 
-  myFunction(image): void {
-    console.log('imageeeee', this.sanitizer.bypassSecurityTrustUrl(image));
-    this.mySrc = this.sanitizer.bypassSecurityTrustUrl(image);
-    console.log('mySrc', this.mySrc);
+  myFunction(image) : void {
+    let mySrc = this.sanitizer.bypassSecurityTrustUrl(image);
+    console.log('imageeeee', mySrc);
   }
 
-  _arrayBufferToBase64(buffer) {
-    let binary = '';
-    let bytes = new Uint8Array(buffer);
-    let len = bytes.byteLength;
-    for (let i = 0; i < len; i++) {
-      binary += String.fromCharCode(bytes[i]);
-    }
-    return window.btoa(binary);
+   _arrayBufferToBase64( buffer ) {
+     var binary = '';
+     var bytes = [].slice.call(new Uint8Array(buffer));
+     bytes.forEach((b) => binary += String.fromCharCode(b));
+     return window.btoa(binary);
   }
 
   openModal(img) {
